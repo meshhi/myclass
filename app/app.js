865af0104319e2433d23db58e89c62f2a32dad19
meshi <mehhi@yandex.ru>
2023-06-01 19:04:50 +0300

    init

--- app/app.js
+++ app/app.js
@@ -2,16 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import rootRouter from "./routes/rootRouter.js";
 import errorMiddleware from "./middleware/errorMiddleware.js";
+import pgClient from "./db/db.js";
 dotenv.config();
 
 const start = async () => {
-  const app = express();
-  const port = process.env.APP_PORT || 3999
-  app.use(express.json());
-  app.use('/api', rootRouter);
-  app.use(errorMiddleware);
-
-  app.listen(port, () => console.log("Listening on port " + port));
+  try {
+    const app = express();
+    const port = process.env.APP_PORT || 3999
+  
+    await pgClient.connect();
+    
+    app.use(express.json());
+    app.use('/api', rootRouter);
+    app.use(errorMiddleware);
+  
+    app.listen(port, () => console.log("Listening on port " + port));
+  } catch (err) {
+    console.log('Error occured while start app!')
+    console.log(err)
+  }
 }
 
 start();
